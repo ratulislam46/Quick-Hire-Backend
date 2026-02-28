80f364c640684c3df6920ce294b8158b77ae12dd
ratulislam46 <arfanratul46@gmail.com>
2026-03-01 03:15:40 +0600

    Delete Job with job id

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
             res.send(result);
         });
 
+        //  Delete Job
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobCollection.deleteOne(query);
+            res.send(result);
+        });
+
         console.log("Successfully connected to MongoDB!");
     } finally {
     }
